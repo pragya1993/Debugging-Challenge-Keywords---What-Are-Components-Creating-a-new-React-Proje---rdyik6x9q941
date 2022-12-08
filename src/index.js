2e412e202b690f7301fd9d2e0a1f491374060e82
Pragya <1993pragyapandey@gamil.com>
2022-12-08 20:56:14 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React, { useEffect, useState } from "react";
+import '../styles/App.css';
+
+const App = () => {
+
+  let [count, setCount] = useState(0)
+
+  return (
+    <div class="ball">
+      <h1 class="count" ondoubleclick={() => { alert("cant edit it") }}>{count}</h1>
+      <button class='increment-button' onclick={() => { setCount(count + 1) }}>Increment</button>
+    </div>
+  )
+}
 
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+export default App;
